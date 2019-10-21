d0b0c47b94b8db8c1e7857cf724a61974ed34b13
efdali <efdal_incesu@hotmail.com>
2019-10-21 18:37:50 +0300

    postlar ve post_detay düzenleme

--- api/post_detay.php
+++ api/post_detay.php
@@ -7,45 +7,19 @@ require_once("../sistem/ayar.php");
 $link=$_GET["link"];
 
 
-//TODO secilen postu getir
-$post=$db->prepare("select * from post p,uye u,topluluk t on
-                        p.uyeId=u.id and p.toplulukId=t.id 
-                        where p.baslik=:baslik");
+// TODO yorumları dahil et
+$post=$db->prepare("select p.id,p.baslik,p.icerik,p.tarih,u.id,u.kadi,u.resim,t.baslik as topluluk,t.logo from post p,uye u,topluluk t where
+                        p.toplulukId=t.id and u.durum=1 and p.durum=1 and p.seoLink=:link");
 
-                       
-
-$post->bindParam(":baslik",$link);
+$post->bindParam(":link",$link);
 $post->execute();
 
 
 if($row=$post->fetch(PDO::FETCH_ASSOC)){
-    //TODO post durum 1#acık 2#banlı
-
-    if($row["p.durum"==2]){
-        echo json_encode(array(
-            "mesaj" => "bu post engelli bir kullanıcıya ait",
-            "durum" =>0
-        ));
-    }else{
-   
-    //TODO secilen posta kayıtlı yorumları getir.
-    $yorum=$db->prepare("select * from uye u,post p,yorum y on
-                            y.uyeId=u.id and y.postId=p.id and p.uyeId=u.id and p.toplulukId=t.id
-                            where y.postId=:p.id 
-                            order by y.tarih desc");
     
-    $yorum->bindParam("p.id:",$row["p.id"]);
-    $yorum->execute();
-    //Efdal! burada if yapmam gerekiyor mu? sonucta hic yorum yoksa da basarısız olmus sayılmaz.
-    $toplamYorum=$yorum->fetchAll(PDO::FETCH_ASSOC);
- 
     echo json_encode(array(
-        "post_detay" => $post,
-        "yorum" => $yorum,
-        "toplamYorum" => $toplamYorum
+        "post_detay" => $row
     ));
-
-}
 
 }else{
     echo json_encode(array(
--- api/postlar.php
+++ api/postlar.php
@@ -6,35 +6,22 @@ $sayfa=@$_GET["s"] ? @$_GET["s"] : 1;
 $limit=10;
 
 $offset=($sayfa-1)*$limit;
-$sorgu=$db->prepare("select p.id,p.baslik,p.icerik,p.tarih,t.id,t.baslik,t.logo,u.id,u.kadi,u.resim
-                                    from post p, topluluk t, uye u
-                                    where p.toplulukId=k.id and p.uyeId=u.id and u.durum!=2  order by p.tarih desc 
-                                    limit $offset,$limit ");
+$sorgu=$db->prepare("select p.*,t.id,t.baslik as topluluk,t.logo,u.kadi,u.resim,COUNT(y.id) as yorum
+                from post p left JOIN 
+                (select * from yorum where yorum.durum!=0) y on p.id=y.postId 
+                INNER JOIN topluluk t on p.toplulukId=t.id
+                INNER JOIN uye u on p.uyeId=u.id where u.durum =1 and p.durum!=0
+                GROUP BY y.postId order by p.tarih DESC 
+                limit $offset,$limit");
+
 $sorgu->execute();
 $basliklar=$sorgu->fetchAll(PDO::FETCH_ASSOC);
-$toplam=$sorgu->rowCount();
 
-
-// TODO yorum sayısı eklencek
-if($toplam){
-    foreach($toplam as $row){
-        $yorum=$db->prepare("select * from uye u,post p,yorum y on
-                            y.uyeId=u.id and y.postId=p.id and p.uyeId=u.id and p.toplulukId=t.id
-                            where y.postId=:p.id ");
-    
-    $yorum->bindParam("p.id:",$row["p.id"]);
-    $yorum->execute();
-    $yorums=$yorum->fetchAll(PDO::FETCH_ASSOC);
-    $yorumSayisi=$yorum->rowCount();
-
-    }
-    
-}
-
+$toplamSayfa=$db->prepare("select COUNT(*) from post");
+$toplamSayfa->execute();
+$toplam=$toplamSayfa->fetch(PDO::FETCH_ASSOC);
 
 echo json_encode(array(
     "postlar"=>$basliklar,
-    "toplam"=>$toplam,
-    "yorumSayi"=>$yorumSayi
+    "toplam"=>$toplam["COUNT(*)"]
 ));
-
